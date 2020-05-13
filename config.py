669be3fe23161a94edd93effe34cac84a2b72dd2
AntonyIS <antonyshikubu@gmail.com>
2020-05-14 02:42:07 +0300

    Added a task scheduler

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import redis
 
 
 class Config(object):
-    ENV = 'dev'
+    ENV = 'prod'
     if ENV == 'dev':
         debug = True
         SQLALCHEMY_DATABASE_URI = "postgresql://postgres:GeoDev@localhost/covidapi"
