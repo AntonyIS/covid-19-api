c60703fe26ad0e712f774463698ef7f4b09676c4
AntonyIS <antonyshikubu@gmail.com>
2020-05-19 17:18:03 +0300

    Fixing deployment issues

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def scheduled_tasks():
     tasks.job()
 
 if __name__ == '__main__':
-    scheduler.add_job(id='Scheduled task', func=scheduled_tasks, trigger='interval', seconds=300)
+    scheduler.add_job(id='Scheduled task', func=scheduled_tasks, trigger='interval', seconds=500)
     scheduler.start()
     app.run()
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     if ENV == 'dev':
         debug = True
         SQLALCHEMY_DATABASE_URI = "postgresql://postgres:GeoDev@localhost/covidapi"
-    else:
+    elif ENV == 'prod':
         debug = 'False'
         SQLALCHEMY_DATABASE_URI = "postgres://mozhnghyvetbvr:86b2c83ff9c3057e870100932398b53b40bb7d7e212e34d245d82f9008aaeb53@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d438kerhs0i6rc"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
