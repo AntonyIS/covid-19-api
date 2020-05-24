e3aa5d3f952fcfdfad5db895ad505268e41f490f
AntonyIS <antonyshikubu@gmail.com>
2020-05-25 02:52:23 +0300

    Fixed Project's media

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask_apscheduler import APScheduler
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
+
 def scheduled_tasks():
     tasks.job()
 
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(scheduled_tasks, 'interval', minutes=5)
+sched.start()
 
 
 if __name__ == '__main__':
-    sched = BackgroundScheduler(daemon=True)
-    sched.add_job(scheduled_tasks, 'interval', minutes=5)
-    sched.start()
-    scheduler = APScheduler()
     app.run()
