35da489a30deb3946d8b1f4ed34294ceebb42630
AntonyIS <antonyshikubu@gmail.com>
2020-05-25 09:36:46 +0300

    Hid secret code such as db URI

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 from os import environ
-
+from secret import DEV_SQLALCHEMY_DATABASE_URI,PROD_SQLALCHEMY_DATABASE_URI
 
 class Config(object):
     ENV = 'prod'
     if ENV == 'dev':
         debug = True
-        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:GeoDev@localhost/covidapi"
+        SQLALCHEMY_DATABASE_URI = DEV_SQLALCHEMY_DATABASE_URI
     else:
         debug = 'False'
-        SQLALCHEMY_DATABASE_URI = "postgres://mozhnghyvetbvr:86b2c83ff9c3057e870100932398b53b40bb7d7e212e34d245d82f9008aaeb53@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d438kerhs0i6rc"
+        SQLALCHEMY_DATABASE_URI = PROD_SQLALCHEMY_DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
